0a13997d495119152320782d0227b974c67aaf91
mithunr03 <mithunachu80@gmail.com>
2026-07-07 21:10:50 +0530

    type casting

---
+++ TYPE CASTING/what_is_type casting.py
@@ -0,0 +1,18 @@
+# Type Casting Is The process of converting the value of one data type (integer, string, float, etc.) to another data type.
+a=20
+b="achu"
+c=20.22
+c1=True
+
+# as we can see after executing the above program we can able to see the data type of the variables a,b,c and c1
+# using the type() function.
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(c1))
+
+#let's convert a float to integer
+print (int(c))
+print (float(a))
+print (bool(b))
+
