035257d1cd4cb149e2571fc1123c1d8c7f963e24
mithunr03 <mithunachu80@gmail.com>
2026-07-12 19:48:48 +0530

    Arithmetic operations

---
+++ ARITHMETIC EXPRESSIONS/all.py
@@ -0,0 +1,41 @@
+# so here we are going to learn about arithmetic expressions in python.
+# Arithmetic expressions are used to perform mathematical operations on numbers.
+# The basic arithmetic operators in python are +, -, *, /, %, **, and //.
+
+# Addition
+friends = 0
+num = 0
+friends += 1 # this is the same as friends = friends + 1
+num = num + 2
+print(friends) # this will print 1
+print(num) # this will print 2
+
+#Subtraction
+num =1
+num1=2
+print(num1 - num) # this will print 1)
+
+#Multiplication
+num = 2
+num1 = 3
+print(num * num1) # this will print 6
+
+#Exponentiation
+num = 2
+num **= num
+print(num) # this will print 4
+
+#Division
+num = 6
+num1 = 3
+print(num / num1) # this will print 2.0
+
+
+
+#Modulo  
+
+#it  returns the reminder 
+
+reminder = 10
+num = reminder % 2
+print(num)
---
+++ INPUT FUNCTION AND EXAMPLE PROGRAMS/madlibsgame.py
@@ -0,0 +1,8 @@
+# we are going to create a madlibs game that will ask the user for input and then print out a story using their input
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+print(f"i went to a park and saw a {adjective1} {noun1} {verb1} across the {noun2}. The {noun2} was very {adjective2} and {verb2} all around.")
