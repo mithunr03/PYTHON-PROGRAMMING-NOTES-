227f80bdd7114a6b1ca37dc6ffe2f5e4e1679619
mithunr03 <mithunachu80@gmail.com>
2026-07-12 20:05:08 +0530

    functions

---
+++ ARITHMETIC EXPRESSIONS/round function.py
@@ -0,0 +1,29 @@
+#The result function is used to round off a number it will display the output without the floating pointers 
+
+a = 10.60
+result = round(a)
+print(result)
+
+#absolute value function 
+# the abs function is used to find the distance from a negative number to 0
+
+x = -10
+res = abs(x)
+print(res)
+
+# pow function it is used to find the power 
+
+res = pow(4 , 2) # 4x4=16 
+print(res)
+
+# max function it is used to find the biggest number 
+# min function it is used to find the smallest number
+
+a1 = 10
+b1 = 20
+c1 = 30 
+
+result1 =max(a1,b1,c1)
+result2 =min(a1,b1,c1)
+print(f"the largest number is {result1}")
+print(f"the smallest number is {result2}")
